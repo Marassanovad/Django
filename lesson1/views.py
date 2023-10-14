9c4c57590042dc8a31a0a51eef9e7aa689c22179
Marassanovad <maradasha@mail.ru>
2023-10-14 22:20:25 +0600

    HW_1

---
+++ lesson1/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+import logging
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, filename="lesson1.log", filemode='a', format='%(levelname)s %(message)s')
+
+def index(request):
+    logger.info('Пользователь успешно зашел: '+ str(datetime.now()))
+    return HttpResponse("Hello, world!")
+
+
+def main(request):
+    logger.info('Пользователь успешно зашел: '+ str(datetime.now()))
+    return render(request, "main.html")
+
+def about(request):
+    logger.info('Пользователь успешно зашел: '+ str(datetime.now()))
+    return render(request, "about.html")
